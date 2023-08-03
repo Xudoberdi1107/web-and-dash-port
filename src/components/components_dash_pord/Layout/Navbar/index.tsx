d151c73b6c484a00b353fe656657491ef0a77fd6
Xudoberdi1107 <tojiboyevxudoberdi1107@gmail.com>
2023-08-03 21:05:40 +0500

    commit xudoberdi

---
+++ src/components/components_dash_pord/Layout/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import n from "./style.module.scss"
+
+function Nav(){
+    return(
+        <div className={n.nav}>
+            <h1>Nav</h1>
+
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/components/components_dash_pord/Layout/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import sb from "./style.module.scss"
+
+function Sidebar() {
+    return (
+        <div>
+            <h1>Sidebar</h1>
+        </div>
+    )
+}
+
+export default Sidebar
